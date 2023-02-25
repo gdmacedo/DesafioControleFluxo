c22c99bd3e521285e665964552a8067f770e50f7
Glener Diniz Macedo <gdmacedo@hotmail.com>
2023-02-25 15:25:25 -0300

    Atualização Inicial

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+/********************************************************************
+*Esta Classe representará a exceção de negócio no sistema
+*********************************************************************/
+public class ParametrosInvalidosException extends Exception{
+
+}
